41a653ff78954b3bce28c8fe0ece07d12696fff8
stephan <stephansmit@hotmail.com>
2019-01-23 14:16:53 +0100

    workning version

--- SimplePointsReader.cxx
+++ SimplePointsReader.cxx
@@ -19,187 +19,126 @@
 
 #include <lines.h>
 #include <vtkpoint.h>
-
-//class INDEXED_POINT : public POINT
-//{
-//public:
-//	int i;
-////	double x,y,z;
-//public:
-//    INDEXED_POINT(double x_, double y_, double z_, int i_): POINT(x_,y_,z_), i(i_) {}
-//};
-//
-//bool sortByXYZ(const INDEXED_POINT &lhs, const INDEXED_POINT &rhs) {
-//
-//    if (lhs.x < rhs.x)
-//        return true;
-//    else if (lhs.x > rhs.x)
-//        return false;
-//    if (lhs.y  < rhs.y)
-//        return true;
-//    else if (lhs.y > rhs.y)
-//        return false;
-//    if (lhs.z  < rhs.z)
-//        return true;
-//    else if (lhs.z > rhs.z)
-//        return false;
-//}
+#include <Param.h>
 
 
+void output_results(string VTKFile, string OutPutFile){
 
-//deque<INDEXED_POINT> sort_by_distance(deque<POINT>, pt){
-//
-//}
-//struct point
-//{
-//    // Left out making a constructor for simplicity's sake.
-//    double x;
-//    double y;
-//    double z;
-//    int i;
-//};
-//
-//
-//
+	//read all the data from the file
+	vtkSmartPointer<vtkUnstructuredGridReader> reader =
+	vtkSmartPointer<vtkUnstructuredGridReader>::New();
+	reader->ReadAllScalarsOn();
+	reader->ReadAllVectorsOn();
+	reader->SetFileName(VTKFile.c_str());
+	reader->Update();
+
+	//read the points and the scalars
+	vtkUnstructuredGrid* ugrid = reader->GetOutput();
+	vtkPointData *pointData = ugrid->GetPointData();
+	vtkDataArray *arrTemperature = pointData->GetArray("Temperature");
+	vtkDataArray *arrPressure = pointData->GetArray("Pressure");
+	vtkDataArray *arrDensity = pointData->GetArray("Density");
+	vtkDataArray *arrXMomentum = pointData->GetArray("X-Momentum");
+	vtkDataArray *arrYMomentum = pointData->GetArray("Y-Momentum");
+	vtkDataArray *arrZMomentum = pointData->GetArray("Z-Momentum");
+	vtkDataArray *arrXGrid = pointData->GetArray("Grid_Velx");
+	vtkDataArray *arrYGrid = pointData->GetArray("Grid_Vely");
+	vtkDataArray *arrZGrid = pointData->GetArray("Grid_Velz");
+	vtkDataArray *arrSoundSpeed = pointData->GetArray("SoundSpeed");
+	vtkDataArray *arrEntropy = pointData->GetArray("Entropy");
+	vtkDataArray *arrMach = pointData->GetArray("Mach");
+
+	//create the VTK points
+	double tmp_pt[3];
+	deque<VTKPOINT> deq;
+	for (int i = 0; i < ugrid->GetNumberOfPoints(); i++)
+	{
+	  ugrid->GetPoint(i,tmp_pt);
+	  VTKPOINT pt = VTKPOINT(
+			  tmp_pt[0],
+			  tmp_pt[1],
+			  tmp_pt[2],
+			  arrTemperature->GetTuple1(i),
+			  arrPressure->GetTuple1(i),
+			  arrDensity->GetTuple1(i),
+			  arrXMomentum->GetTuple1(i),
+			  arrYMomentum->GetTuple1(i),
+			  arrZMomentum->GetTuple1(i),
+			  arrXGrid->GetTuple1(i),
+			  arrYGrid->GetTuple1(i),
+			  arrZGrid->GetTuple1(i),
+			  arrSoundSpeed->GetTuple1(i),
+			  arrEntropy->GetTuple1(i),
+			  arrMach->GetTuple1(i)
+	  );
+	  deq.push_back(pt);
+	};
+	//sort on the basis of coordinates
+	sort(deq.begin(), deq.end(), sortByXYZ);
+
+	//split in different spanheights
+	deque<VTKPOINT> tmpdeq1,tmpdeq2,tmpdeq3;
+	for (int i=0; i<deq.size();i++){
+	  tmpdeq1.push_back(deq[i]);
+	  i++;
+	  tmpdeq2.push_back(deq[i]);
+	  i++;
+	  tmpdeq3.push_back(deq[i]);
+	}
+
+	//read leading point
+	POINT leadPt = readPt("leadpoint.txt");
+
+
+	//order the points
+	deque<VTKPOINT> order_deq1, init_order_deq1;
+	deque<VTKPOINT> order_deq2, init_order_deq2;
+	deque<VTKPOINT> order_deq3, init_order_deq3;
 
 
 
+	initialize_ordering(init_order_deq1, tmpdeq1, leadPt);
+	initialize_ordering(init_order_deq2, tmpdeq2, leadPt);
+	initialize_ordering(init_order_deq3, tmpdeq3, leadPt);
+
+
+	order_deq1 =order_by_distance(init_order_deq1, tmpdeq1);
+	order_deq2 =order_by_distance(init_order_deq2, tmpdeq2);
+	order_deq3 =order_by_distance(init_order_deq3, tmpdeq3);
+
+
+	//create splines
+	VTKSPLINE test1 = VTKSPLINE(order_deq1);
+	VTKSPLINE test2 = VTKSPLINE(order_deq2);
+	VTKSPLINE test3 = VTKSPLINE(order_deq3);
+
+	//write the points
+	string file1 = OutPutFile+"_1.txt";
+	string file2 = OutPutFile+"_2.txt";
+	string file3 = OutPutFile+"_3.txt";
+
+	//write the points
+	test1.writeCtrPts(file1.c_str());
+	test2.writeCtrPts(file2.c_str());
+	test3.writeCtrPts(file3.c_str());
+
+
+}
 int main(int argc, char* argv[])
 {
-  // Verify input arguments
-  if ( argc != 2 )
-  {
-    std::cout << "Usage: " << argv[0]
-              << " Filename(.xyz)" << std::endl;
-    return EXIT_FAILURE;
-  }
- 
-  //read all the data from the file
-  vtkSmartPointer<vtkUnstructuredGridReader> reader =
-  vtkSmartPointer<vtkUnstructuredGridReader>::New();
-  reader->ReadAllScalarsOn();
-  reader->ReadAllVectorsOn();
-  reader->SetFileName(argv[1]);
-  reader->Update();
 
+	ParamMap param = ParamMap(argv[1]);
+	string VTKPRESSURE = param.getStringParam("VTKFILE_PRESSURE");
+	string VTKSUCTION = param.getStringParam("VTKFILE_SUCTION");
+	string LEADPOINT = param.getStringParam("TXTFILE_LEADPOINT");
+	string OUTPUTPRESSURE =param.getStringParam("OUTPUTFILE_PRESSURE");
+    string OUTPUTSUCTION =param.getStringParam("OUTPUTFILE_SUCTION");
 
+    cout << "reading the results" << endl;
+    output_results(VTKPRESSURE, OUTPUTPRESSURE);
+    output_results(VTKSUCTION, OUTPUTSUCTION);
+    cout << "written the results" << endl;
 
-
-  vtkUnstructuredGrid* ugrid = reader->GetOutput();
-  vtkPointData *pointData = ugrid->GetPointData();
-
-  vtkDataArray *arr = pointData->GetArray("Temperature");
-
-  double vec[3];
-  deque<VTKPOINT> deq;
-  for (int i = 0; i < ugrid->GetNumberOfPoints(); i++)
-  {
-      ugrid->GetPoint(i,vec);
-//      cout << vec[0] << endl;
-      VTKPOINT pt = VTKPOINT(vec[0],vec[1],vec[2],i,arr->GetTuple1(i));
-//      cout << pt.x << endl;
-      deq.push_back(pt);
-  };
-  sort(deq.begin(), deq.end(), sortByXYZ);
-
-  for (int i=0; i<deq.size();i++){
-	  deq[i].print();
-	  i++;
-	  i++;
-  }
-  deque<POINT> tmpdeq;  for (int i=0; i<deq.size();i++)  tmpdeq.push_back((POINT) deq[i]);
-
-
-  SPLINE test = SPLINE(tmpdeq);
-  test.writeCtrPts("without.txt");
-
-  deque<POINT> tmpwrite = test.discretize(1000,true,true);
-  SPLINE spline(tmpwrite);
-  spline.writeCtrPts("test.txt");
-//  addToDisplay(test);
-//
-
-
-
-
-  //
-  //  vtkCellData *cellData = ugrid->GetCellData();
-  //  vtkPointData *pointData = ugrid->GetPointData();
-  //  vtkFieldData *fieldData = ugrid->GetFieldData();
-  //
-  //  cout << ugrid->GetNumberOfPoints();
-  //  double test = pointData->GetArray("Temperature")->GetTuple1(2);
-
-//
-//
-
-//  for (int i = 0; i < fieldData->GetNumberOfArrays(); i++)
-//  {
-//      vtkDataArray* data = fieldData->GetArray(i);
-//  };
-//
-//
-//  for (int i = 0; i < cellData->GetNumberOfArrays(); i++)
-//    {
-//        vtkDataArray* data = cellData->GetArray(i);
-//        cout << "cell " << data->GetName() << endl;
-//
-//    };
-//  for (int i = 0; i < pointData->GetNumberOfArrays(); i++)
-//	{
-//		vtkDataArray* data = pointData->GetArray(i);
-//		//vtkLookupTable* lut= data->GetLookupTable();
-//
-//		cout << "point " << data->GetName() << endl;
-//		// =  data->;
-//		//cout << test << endl;
-//	};
-
-
-//      for (int j = 0; j < data->GetNumberOfTuples(); j++)
-//      {
-//          double value = data->GetTuple1(j);
-//          cout << "  value " << j << "th is " << value << endl;
-//      }
-
-
-//  vtkSmartPointer<vtkSimplePointsReader> reader =
-//    vtkSmartPointer<vtkSimplePointsReader>::New();cmake example vtk
-//  reader->SetFileName ( argv[1] );
-//  reader->Update();
-//
-  // Visualize
-
-
-  //Create a mapper and actor
-//  vtkSmartPointer<vtkDataSetMapper> mapper =
-//    vtkSmartPointer<vtkDataSetMapper>::New();
-//  mapper->SetInputConnection(reader->GetOutputPort());
-//
-//  vtkSmartPointer<vtkActor> actor =
-//    vtkSmartPointer<vtkActor>::New();
-//  actor->SetMapper(mapper);
-//
-//  //Create a renderer, render window, and interactor
-//  vtkSmartPointer<vtkRenderer> renderer =
-//    vtkSmartPointer<vtkRenderer>::New();
-//  vtkSmartPointer<vtkRenderWindow> renderWindow =
-//    vtkSmartPointer<vtkRenderWindow>::New();
-//  renderWindow->AddRenderer(renderer);
-//  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-//    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-//  renderWindowInteractor->SetRenderWindow(renderWindow);
-//
-//  //Add the actor to the scene
-//  renderer->AddActor(actor);
-//  renderer->SetBackground(.3, .6, .3); // Background color green
-//
-//  //Render and interact
-//  renderWindow->Render();
-//  renderWindowInteractor->Start();
-//
-//
-//  renderWindowInteractor->Start();
 
   return EXIT_SUCCESS;
 }
