cc7cf26d8bae77a5859976961c378aeb70d20f17
Stephan Smit <stephans@int2.bullx>
2019-01-24 13:28:48 +0100

    changed

--- make.py
+++ make.py
@@ -1,17 +1,22 @@
 import os
 
-#check where it is run
+#compile on cluster
 if ("stephans" in os.environ['HOME']):
-   os.system("module load vtk/6.2.0")
-   os.system("module load gcc/5.2.0")
-   os.system("mkdir -p build")
-   os.chdir("build")
-   os.system("cmake -DMESHGEN_SRC:PATH=/home/stephans/Blade-MeshGen/src -DSRC_DIR=/home/stephans/postprocessing-su2-surface .. -DSISL_DIR:PATH=/home/stephans/c-libraries/include/ -DSISL_LIB=/home/stephans/c-libraries/lib/libsisl.a ..")
-   os.system("make -j8")
+   cmds =[ 
+   "module load cmake",
+   "module load vtk/6.2.0",
+   "module load gcc/5.2.0",
+   "mkdir -p build",
+   "(cd build && cmake -DMESHGEN_SRC:PATH=/home/stephans/Blade-MeshGen/src -DSRC_DIR=/home/stephans/postprocessing-su2-surface .. -DSISL_DIR:PATH=/home/stephans/c-libraries/include/ -DSISL_LIB=/home/stephans/c-libraries/lib/libsisl.a ..)",
+   "(cd build && make -j8)"
+   ]
+#compile local
 else:
-   os.system("mkdir -p build")
-   os.chdir("build")
-   os.system("cmake -DVTK_DIR:PATH=/home/azureuser/Documents/VTK/build -DMESHGEN_SRC:PATH=/home/azureuser/Documents/Blade-MeshGen/src -DSRC_DIR=/home/azureuser/Documents/postprocessing-su2-surface ..")
-   os.system("make -j8")
+   cmds = [
+    "mkdir -p build",
+    "cd build && cmake -DVTK_DIR:PATH=/home/azureuser/Documents/VTK/build -DMESHGEN_SRC:PATH=/home/azureuser/Documents/Blade-MeshGen/src -DSRC_DIR=/home/azureuser/Documents/postprocessing-su2-surface ..",
+    "cd build && make -j8"
+   ]
 
+os.system("\n".join(cmds))
 
